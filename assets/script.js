3cb1595aca4220099da8b1e9386c001a11342924
kolatham <92490383+kolatham@users.noreply.github.com>
2021-11-20 18:56:08 -0500

    added another html and js

--- assets/script.js
+++ assets/script.js
@@ -1,173 +1,111 @@
+const question =document.querySelector("#question");
+const choices =Array.from(document.querySelectorAll(".choice-text"));
+const progressText =document.querySelector("#progressText");
+const scoreTest =document.querySelector("#score");
 
-var timerEl =document.querySelector("#timer")
-var scoreText =document.querySelector("#score")
-var secondsLeft = 140;
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let availableQuestions =[]
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
-    {
-        question: "How do you make a button clickable?",
-        answers: {
-            a:"console.log",
-            b:"addEventListener",
-            c:"btn",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a:"",
-            b:"",
-            c:"",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a:"",
-            b:"",
-            c:"",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a:"",
-            b:"",
-            c:"",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a:"",
-            b:"",
-            c:"",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a:"",
-            b:"",
-            c:"",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-        answers: {
-            a:"",
-            b:"",
-            c:"",
-            d: "",
-        },
-        correctAnswer: ""
-    },
-];
 
-function beginQuiz() {
-    questionCounter = 0;
-    score =0;
-    nextQuestions =[...myQuestions];
-    getNextQuestion();
+let questions = [
+    {
+        question: "What is...",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+    },
+    {
+        question: "What is...",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+    },
+    {
+        question: "What is...",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+    },
+    {
+        question: "What is...",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+    },
+    {
+        question: "What is...",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+    },
+]
+const score_points = 10
+const max_questions = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
 
-function nextQuestion() {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...myQuestions];
-    getNextQuestion();
-}
-
-function buildQuiz(){
-const output =[];
-myQuestions.forEach(
-    (currentQuestion, questionNumber) 
-  
-)
-const answers = [];
-for(letter in currentQuestion.answers) {
-    answers.push(
-        <label>
-            <input type ="radio" name ="question${questionNumber}" value ="${letter}"></input>
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-        </label>
-
-    );
-    output.push(
-        <div class="slide">
-        <div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>
-    </div>
-quizContainer.innerHTML = output.join(""),
-
-function setTime() {
-    Headers.style.display = "none";
-    startBtn.style.display ="none";
-    var timerInterval = setInterval(function( {
-        secondsLeft---;
-        timerEl.textContent = secondsLeft + "seconds remaining.";
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-
-    
-}, 1000);
-};
-
-
-function showResults() {
-    const answerContainers=quizContainer.querySelectorAll(".answers");
-    let numCorrect = 0;
-    myQuestions.forEach(currentQuestion, questionNumber) ;
-        const answerContainer = answerContainers[questionNumber];
-        const selector ="input[name=question${questionNumber}]:checked";
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        if (userAnswer === currentQuestion.correctAnswer) {
-            numCorrect++;
-            answerContainers[questionNumber].style.color = "emerald green";
-        }
-        else {
-            answerContainers[questionNumber].style.color = "ruby red";
-        }
+getNewQuestion = () =>{
+    if(availableQuestions.length === 0 || questionCounter >max_questions) {
+        localStorage.setItem("mostRecentScore", score)
+        return window.location.assign("/end.html")
     }
-    resultsContainer.innerHTML = "${numCorrect} out of ${myQuestions.length}",
 
-    
-    function showSlide(n) {
-        slides[currentSlide].classList.remove("active-slide");
-        slides[n].classList.add("active-slide");
-        currentSlide = n;
-        if(currentSlide === slides.length-1) {
-            nextButton.style.display = "none";
-            submitButton.style.display ="inline-block"
-        }
-        else{
-            nextButton.style.display = "inline-block";
-            submitButton.style.display ="none";
-        }
+    questionCounter++
+    progressText.innerText = "Question ${questionCounter} of ${max_questions}"
+
+    const questionsIndex = Math.floor(Math.random()*availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"]
+        choice.innerText = currentQuestion ["choice" + number]
+    })
+
+    availableQuestions.splice(questionIndex,1)
+
+    acceptingAnswers = true
+
+}
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e =>{
+        if(!acceptingAnswers) return
         
-    }
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset["number"]
+        
+        let classToApply = selectAnswer ==currentQuestion.answer ? "correct" :
+        "incorrect"
 
-nextButton.addEventListener("click", showNextSlide);
-submitButton.addEventListener('click', showResults);
-beginButton.addEventListener("click", beginQuiz);
+        if(classToApply === "correct") {
+            incrementScore(score_points)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+        setTimeout(()=>{
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+        
+    })
+})
+
+incrementScore =num =>{
+    score +=numscoreText.innerText = score
 }
 
-z
+startGame()
