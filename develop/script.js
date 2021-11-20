4a31cc66f1902308ca679ef8b08e660c3e350e62
kolatham <92490383+kolatham@users.noreply.github.com>
2021-11-19 19:46:54 -0500

    added to the html and added the question section to javascript

--- develop/script.js
+++ develop/script.js
@@ -1,14 +1,106 @@
-function buildQuiz(){}
-function showResults(){}
+f
 
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
-const myQuestions = [];
+const myQuestions = [
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a:"",
+            b:"",
+            c:"",
+            d: "",
+        },
+        correctAnswer: ""
+    },
+];
 
-buildQuiz();
+function buildQuiz(){
+const output =[];
+myQuestions.forEach(
+    (currentQuestion, questionNumber) 
+  
+)
+const answers = [];
+for(letter in currentQuestion.answers) {
+    answers.push(
+        <label>
+            <input type ="radio" name ="question${questionNumber}" value ="${letter}"></input>
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+        </label>
 
-submitButton.addEventListener('click', showResults);
+    );
+    output.push(
+        <div class = "question"> ${currentQuestion}</div>
+        );
+    output.push(
+        <div class = "question">${answers.join("")}</div>
+    );
+quizContainer.innerHTML = output.join("");
+ }
+
 
 function showResults() {
     const answerContainers=quizContainer.querySelectorAll(".answers");
@@ -24,6 +116,32 @@ function showResults() {
         else {
             answerContainers[questionNumber].style.color = "ruby red";
         }
-    };
+    }
     resultsContainer.innerHTML = "${numCorrect} out of ${myQuestions.length}";
 
+    
+    function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+        if(currentSlide === slides.length-1) {
+            nextButton.style.display = "none";
+            submitButton.style.display ="inline-block"
+        }
+        else{
+            nextButton.style.display = "inline-block";
+            submitButton.style.display ="none";
+        }
+        
+    }
+    
+    showSlide(currentSlide);
+
+    function showNextSlide() {
+        showSlide(currentSlide +1);
+    
+    }    
+
+nextButton.addEventListener("click", showNextSlide);
+submitButton.addEventListener('click', showResults);
+}
